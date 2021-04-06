949e9890907800955513f8099eacbbdedaac6fee
Axel Collard Bovy <axcbovy@gmail.com>
2021-04-06 11:13:40 -0300

    Initial Commit

---
+++ MeLiCatalog/MeLiCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeLiCatalogApp.swift
+//  MeLiCatalog
+//
+//  Created by Axel Collard Bovy on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct MeLiCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
